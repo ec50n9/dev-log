cbafcdfebc145c8342d3692b304db6f5fb3fd2b6
ec50n9 <shelloworld@qq.com>
2025-10-24 17:07:13 +0800

    fix

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,14 +1,10 @@
 import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
 import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(utc);
-dayjs.extend(relativeTime);
-dayjs.locale('zh-cn');
 
 // 北京时间是 UTC+8，即偏移 8 小时 = 480 分钟
 export function getBeijingTime(date?: any) {
-  if (date) return dayjs(date).utcOffset(480);
-  return dayjs().utcOffset(480);
+  return (date ? dayjs(date) : dayjs()).add(8, 'h');
 }
 
